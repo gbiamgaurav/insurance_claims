c4926ca1c7229eb05cbd150bfd4a326eb2adaf5e
gbiamgaurav <gaurav.bhattacharya10@gmail.com>
2023-06-01 12:08:20 +0530

    data cleaning done

---
+++ src/components/data_cleaning.py
@@ -0,0 +1,52 @@
+import sys
+from dataclasses import dataclass
+import numpy as np 
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
+from src.exception import CustomException
+from src.logger import logging
+import os
+from src.utils import save_object
+from sklearn.model_selection import train_test_split
+
+
+@dataclass
+class DataCleaningConfig:
+    train_data_path_cleaned: str = os.path.join("artifacts", "train_cleaned.csv")
+    test_data_path_cleaned: str = os.path.join("artifacts", "test_cleaned.csv")
+
+class DataCleaning:
+    def __init__(self):
+        self.cleaning_config = DataCleaningConfig()
+
+    def initiate_data_cleaning(self):
+        logging.info("Entered the data cleaning method or component")
+        try:
+            df1 = pd.read_csv("artifacts/train.csv")
+            df2 = pd.read_csv("artifacts/test.csv")
+
+            logging.info("Read the dataset as dataframe")
+
+            os.makedirs(os.path.dirname(self.cleaning_config.train_data_path_cleaned), exist_ok=True)
+            logging.info("directory created")
+
+            df1["fraud_reported"].replace({"N": 0, "Y": 1}, inplace=True)
+            df2["fraud_reported"].replace({"N": 0, "Y": 1}, inplace=True)
+
+            df1.to_csv(self.cleaning_config.train_data_path_cleaned,index=False,header=True)
+            logging.info('df1 saved')
+
+            df2.to_csv(self.cleaning_config.test_data_path_cleaned,index=False,header=True)
+            logging.info('df2 saved')
+
+            logging.info("Train test data cleaned")
+
+            return df1, df2
+
+            logging.info("returned df1 and df2")
+
+        except Exception as e:
+            raise CustomException(e, sys)
--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,84 +1,69 @@
 import os
 import sys
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from dataclasses import dataclass
-import warnings
-warnings.filterwarnings("ignore")
-from joblib import dump
-
 from src.exception import CustomException
 from src.logger import logging
-from src.components.data_transformation import DataTransformation, DataTransformationConfig
-from src.components.model_training import ModelTrainerConfig, ModelTrainer
-from src.utils.utils import save_object, load_object, evaluate_models
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+from src.components.data_cleaning import DataCleaning
+
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str = os.path.join("artifacts", "train.csv")
-    test_data_path: str = os.path.join("artifacts", "test.csv")
-    raw_data_path: str = os.path.join("artifacts", "raw.csv")
+    train_data_path: str=os.path.join('artifacts',"train.csv")
+    test_data_path: str=os.path.join('artifacts',"test.csv")
+    raw_data_path: str=os.path.join('artifacts',"data.csv")
 
 class DataIngestion:
     def __init__(self):
-        self.ingestion_config = DataIngestionConfig()
+        self.ingestion_config=DataIngestionConfig()
 
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
             df = pd.read_csv("notebook/data/cleaned_data.csv")
-            logging.info("Read the dataset as a dataframe")
+            logging.info('Read the dataset as dataframe')
+            
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
 
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
 
-            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
-            logging.info("Train-test split initiated")
+            logging.info("Train test split initiated")
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
 
-            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
 
-            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
-            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+
+    
 
             logging.info("Ingestion of the data is completed")
 
-            return (
-                self.ingestion_config.train_data_path,
-                self.ingestion_config.test_data_path,
-                self.ingestion_config.raw_data_path
-            )
+            return self.ingestion_config.raw_data_path
+            return self.ingestion_config.train_data_path
+            return self.ingestion_config.test_data_path
 
+
+            
         except Exception as e:
-            raise CustomException(e, sys)
+            raise CustomException(e,sys)
 
 
-if __name__ == "__main__":
-    obj = DataIngestion()
-    train_path, test_path, _ = obj.initiate_data_ingestion()
-
-    data_transformation = DataTransformation()
-
-    try:
-        train_arr, test_arr, preprocessor_obj = data_transformation.initiate_data_transformation(train_path, test_path)
-    except CustomException as e:
-        logging.error(str(e))
-        sys.exit(1)
-
-    # Save the preprocessor_obj
-    try:
-        os.makedirs("artifacts", exist_ok=True)
-        preprocessor_path = os.path.join("artifacts", "preprocessor.pkl")
-        dump(preprocessor_obj, preprocessor_path)
-        logging.info("Preprocessor object saved successfully")
-    except Exception as e:
-        logging.error("Failed to save the preprocessor object: " + str(e))
         
-    
-    modeltrainer = ModelTrainer()
-    try:
-        acc_score, p_score = modeltrainer.initiate_model_trainer(train_arr, test_arr)
-        logging.info(f"Accuracy Score: {acc_score}")
-        logging.info(f"Precision Score: {p_score}")
-        
-    except Exception as e:
-        raise CustomException(e, sys)
+if __name__=="__main__":
+    obj=DataIngestion()
+    raw_data_imgestion=obj.initiate_data_ingestion()
+
+    obj2=DataCleaning()
+    raw_arr=obj2.initiate_data_cleaning() 
+
+
+
+
+
+
+
+
+
+
+ 
